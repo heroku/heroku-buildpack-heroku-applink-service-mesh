c22856f3d8081587312d2dac922a44b12f98d22d
Rune Soerensen <rsoerensen@salesforce.com>
2026-01-25 03:39:23 -0500

    Rename test output function

--- test/test-cnb.sh
+++ test/test-cnb.sh
@@ -28,7 +28,7 @@ PLATFORM="linux/${ARCH}"
 
 OUTPUT_IMAGE="applink-cnb-test"
 
-output::test_step() {
+output::info() {
     local ansi_blue=$'\e[1;34m'
     local ansi_reset=$'\e[0m'
     echo -e "${ansi_blue}$*${ansi_reset}"
@@ -36,13 +36,13 @@ output::test_step() {
 
 remove_image() {
     if [ ! -z "$(docker images -q ${OUTPUT_IMAGE})" ]; then
-        output::test_step "Removing test output image '${OUTPUT_IMAGE}'"
+        output::info "Removing test output image '${OUTPUT_IMAGE}'"
         docker rmi "${OUTPUT_IMAGE}" &> /dev/null || true
     fi
 }
 
 run_build() {
-    output::test_step "Running pack to build '${FIXTURE}' with CNB using '${BUILDER}' on '${PLATFORM}'"
+    output::info "Running pack to build '${FIXTURE}' with CNB using '${BUILDER}' on '${PLATFORM}'"
     pack build "${OUTPUT_IMAGE}" \
         --builder "${BUILDER}" \
         --platform "${PLATFORM}" \
@@ -50,7 +50,7 @@ run_build() {
         --trust-extra-buildpacks \
         --path "${FIXTURE}"
 
-    output::test_step "Running 'heroku-applink-service-mesh -v' on output image '${OUTPUT_IMAGE}'"
+    output::info "Running 'heroku-applink-service-mesh -v' on output image '${OUTPUT_IMAGE}'"
     docker run \
         --rm "$OUTPUT_IMAGE" \
         -- heroku-applink-service-mesh -v
@@ -60,7 +60,7 @@ remove_image
 
 run_build
 
-output::test_step "Rebuild to test caching"
+output::info "Rebuild to test caching"
 run_build
 
 remove_image
